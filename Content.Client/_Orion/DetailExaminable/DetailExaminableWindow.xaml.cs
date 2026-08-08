FluxS-Dev/Amour

[Feature] Examine UI (#165)

Content.Client/_Orion/DetailExaminable/DetailExaminableWindow.xaml.cs
@@ -1,0 +1,330 @@
+using System.Linq;
+using System.Numerics;
+using Content.Client.Stylesheets;
+using Content.Client.UserInterface.Controls;
+using Content.Shared._Orion.DetailExaminable;
+using Content.Shared.CCVar;
+using Content.Shared.Inventory;
+using Robust.Client.AutoGenerated;
+using Robust.Client.Player;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Configuration;
+
+namespace Content.Client._Orion.DetailExaminable;
+
+//
+// License-Identifier: GPL-3.0-or-later
+//
+
+[GenerateTypedNameReferences]
+public sealed partial class DetailExaminableWindow : FancyWindow
+{
+    private readonly List<Direction> _rotationSequence = new()
+    {
+        Direction.East,
+        Direction.North,
+        Direction.West,
+        Direction.South,
+    };
+
+    private int _currentDirectionIndex = 3;
+    private EntityUid? _currentEntity;
+    private SpriteView? _spriteView;
+
+    public DetailExaminableWindow()
+    {
+        RobustXamlLoader.Load(this);
+
+        RotateLeftButton.OnPressed += _ => RotateDirection(-1);
+        RotateRightButton.OnPressed += _ => RotateDirection(1);
+    }
+
+    private void RotateDirection(int step)
+    {
+        _currentDirectionIndex = (_currentDirectionIndex + step + _rotationSequence.Count) % _rotationSequence.Count;
+        UpdateSpriteDirection();
+    }
+
+    private void UpdateSpriteDirection()
+    {
+        if (_currentEntity == null || TargetPreview == null)
+            return;
+
+        TargetPreview.RemoveAllChildren();
+
+        _spriteView = new SpriteView // TODO: Better view for 32x48* sprites
+        {
+            OverrideDirection = _rotationSequence[_currentDirectionIndex],
+            Scale = new Vector2(8f, 8f),
+//            MaxSize = new Vector2(64, 256), // This killing 32x48* sprites
+            MinSize = new Vector2(32, 128),
+            Stretch = SpriteView.StretchMode.Fit,
+        };
+
+        _spriteView.SetEntity(_currentEntity.Value);
+        TargetPreview.AddChild(_spriteView);
+    }
+
+    public void UpdateState(DetailExaminableEuiState state, IEntityManager entManager)
+    {
+        var config = IoCManager.Resolve<IConfigurationManager>();
+        _currentEntity = entManager.GetEntity(state.Target);
+
+        var isClothed = IsCharacterClothed(_currentEntity.Value, entManager);
+
+        PreviewTabs.SetTabTitle(0, Loc.GetString("flavor-tab-flavor"));
+        PreviewTabs.SetTabTitle(1, Loc.GetString("flavor-tab-character"));
+        PreviewTabs.SetTabTitle(2, Loc.GetString("flavor-tab-gyr"));
+        PreviewTabs.SetTabTitle(3, Loc.GetString("flavor-tab-nsfw"));
+
+        // Hide things if they are disabled in CCVars
+        var showTraits = config.GetCVar(CCVars.FlavorTraitsEnabled);
+        var showOoc = config.GetCVar(CCVars.FlavorOocEnabled);
+        var showGyr = config.GetCVar(CCVars.FlavorGyrEnabled);
+        var showNsfw = config.GetCVar(CCVars.FlavorNsfwEnabled) &&
+                       config.GetCVar(CCVars.NsfwContentEnabled) &&
+                       !isClothed;
+        var showLinks = config.GetCVar(CCVars.FlavorLinksEnabled);
+
+        PreviewTabs.SetTabVisible(0, showOoc);
+        PreviewTabs.SetTabVisible(1, showTraits);
+        PreviewTabs.SetTabVisible(2, showGyr);
+        PreviewTabs.SetTabVisible(3, showNsfw);
+
+        var species = Loc.GetString($"species-name-{state.Species.ToString().ToLower()}");
+        var sex = Loc.GetString($"humanoid-profile-editor-sex-{state.Sex.ToString().ToLower()}-text");
+        var gender = Loc.GetString($"humanoid-profile-editor-pronouns-{state.Gender.ToString().ToLower()}-text");
+
+        PreviewNameText.Text = state.Name;
+        PreviewGenderText.Text = $"{species} | {sex} | {gender}";
+
+        UpdateSpriteDirection();
+
+        PreviewAppearanceText.SetMessage(GetContentWithEmptyMessage(state.FlavorText, "detail-examinable-empty-flavor"));
+
+        if (showOoc)
+            PreviewOOCText.SetMessage(GetContentWithEmptyMessage(state.OOCFlavorText, "detail-examinable-empty-ooc"));
+
+        if (showTraits)
+            PreviewTraitsText.SetMessage(GetContentWithEmptyMessage(state.CharacterFlavorText, "detail-examinable-empty-character"));
+
+        if (showNsfw)
+            PreviewNSFWText.SetMessage(GetContentWithEmptyMessage(state.NSFWFlavorText, "detail-examinable-empty-nsfw"));
+
+        if (showGyr)
+        {
+            PreviewGYRContainer.RemoveAllChildren();
+
+            PreviewGYRContainer.AddChild(CreateSectionHeader("humanoid-profile-editor-gyr-green", Color.Green, useStripeBack: true));
+            CreateGYRTextLabel(GetContentWithEmptyMessage(state.GreenFlavorText, "detail-examinable-empty-green"));
+
+            PreviewGYRContainer.AddChild(CreateSectionHeader("humanoid-profile-editor-gyr-yellow", Color.Yellow, useStripeBack: true));
+            CreateGYRTextLabel(GetContentWithEmptyMessage(state.YellowFlavorText, "detail-examinable-empty-yellow"));
+
+            PreviewGYRContainer.AddChild(CreateSectionHeader("humanoid-profile-editor-gyr-red", Color.Red, useStripeBack: true));
+            CreateGYRTextLabel(GetContentWithEmptyMessage(state.RedFlavorText, "detail-examinable-empty-red"));
+        }
+
+        PreviewTagsText.Text = state.TagsFlavorText;
+
+        if (showLinks)
+        {
+            ProcessLinks(state.LinksFlavorText);
+        }
+        else // TODO: Remove all container, now its just remove links
+        {
+            PreviewLinksContainer?.RemoveAllChildren();
+        }
+
+        Badge.Visible = PlayerBadge();
+    }
+
+    private static string GetContentWithEmptyMessage(string content, string emptyMessageKey)
+    {
+        if (string.IsNullOrWhiteSpace(content))
+            return Loc.GetString(emptyMessageKey);
+
+        return content;
+    }
+
+    private void CreateGYRTextLabel(string text)
+    {
+        var label = new RichTextLabel
+        {
+            Text = text + "\n",
+            VerticalExpand = true,
+        };
+
+        PreviewGYRContainer.AddChild(label);
+    }
+
+    private void ProcessLinks(string linksText)
+    {
+        if (PreviewLinksContainer == null)
+            return;
+
+        PreviewLinksContainer.RemoveAllChildren();
+        if (string.IsNullOrEmpty(linksText))
+        {
+            var emptyLabel = new Label
+            {
+                Text = Loc.GetString("detail-examinable-empty-links"),
+                HorizontalExpand = true,
+                HorizontalAlignment = HAlignment.Center,
+                FontColorOverride = Color.Gray
+            };
+            PreviewLinksContainer.AddChild(emptyLabel);
+            return;
+        }
+
+        var links = linksText.Split(new[] { ',', ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var link in links)
+        {
+            if (IsValidUrl(link))
+            {
+                CreateLinkButton(link);
+            }
+            else
+            {
+                CreateLinkTextLabel(link);
+            }
+        }
+    }
+
+    private static bool IsValidUrl(string url)
+    {
+        return url.StartsWith("https://docs.google.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://disk.yandex.by", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://disk.yandex.ru", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://docs.yandex.by", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://docs.yandex.ru", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://ru.imgbb.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://gyazo.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://i.gyazo.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://e621.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://static1.e621.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://e926.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://static1.e926.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://ibb.co", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://steamcommunity.com/id/", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://steamcommunity.com/sharedfiles", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://images.steamusercontent.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://media.discordapp.net/attachments", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://cdn.discordapp.com/attachments", StringComparison.OrdinalIgnoreCase);
+    }
+
+    private void CreateLinkButton(string url)
+    {
+        var button = new Button
+        {
+            Text = GetLinkDisplayText(url),
+            ToolTip = Loc.GetString("humanoid-profile-editor-link-tooltip", ("url", url)),
+            HorizontalExpand = true,
+            HorizontalAlignment = HAlignment.Center,
+            StyleClasses = { StyleBase.ButtonOpenBoth },
+        };
+
+        button.OnPressed += _ => OpenLink(url);
+
+        PreviewLinksContainer.AddChild(button);
+    }
+
+    private void CreateLinkTextLabel(string text)
+    {
+        var label = new Label
+        {
+            Text = text,
+            HorizontalExpand = true,
+            HorizontalAlignment = HAlignment.Center,
+            FontColorOverride = Color.Gray,
+        };
+
+        PreviewLinksContainer.AddChild(label);
+    }
+
+    private static string GetLinkDisplayText(string url)
+    {
+        if (url.Length > 40)
+        {
+            return url[..37] + "...";
+        }
+        return url;
+    }
+
+    private static void OpenLink(string url)
+    {
+        if (url.StartsWith("www.", StringComparison.OrdinalIgnoreCase))
+            url = "https://" + url;
+
+        var uriOpener = IoCManager.Resolve<IUriOpener>();
+        uriOpener.OpenUri(url);
+    }
+
+    private Control CreateSectionHeader(string text, Color? color = null, bool useStripeBack = true)
+    {
+        var label = new Label
+        {
+            Text = Loc.GetString(text),
+            StyleClasses = { "LabelBig" },
+            Align = Label.AlignMode.Center,
+        };
+
+        if (color.HasValue)
+            label.FontColorOverride = color.Value;
+
+        if (useStripeBack)
+        {
+            var stripe = new StripeBack();
+            stripe.AddChild(label);
+            return stripe;
+        }
+
+        return label;
+    }
+
+    private bool PlayerBadge() // TODO: Something like donator, coder, owner badges
+    {
+        if (_currentEntity == null)
+            return false;
+
+        var playerManager = IoCManager.Resolve<IPlayerManager>();
+        if (!playerManager.TryGetSessionByEntity(_currentEntity.Value, out var session))
+            return false;
+
+        var playerName = session.Name;
+        var ckeyToMatch = playerName.StartsWith("localhost@", StringComparison.OrdinalIgnoreCase)
+            ? playerName.Substring("localhost@".Length)
+            : playerName;
+
+        var ckeyLower = ckeyToMatch.ToLowerInvariant();
+        var grantedCKeys = new[] { "puroslavking" };
+
+        return grantedCKeys.Contains(ckeyLower);
+    }
+
+    private bool IsCharacterClothed(EntityUid entity, IEntityManager entManager) // Hide NSFW flavor if character is clothed
+    {
+        if (entity == EntityUid.Invalid)
+            return false;
+
+        var inventorySystem = entManager.System<InventorySystem>();
+
+        return inventorySystem.TryGetSlotEntity(entity, "outerClothing", out _) ||
+               inventorySystem.TryGetSlotEntity(entity, "jumpsuit", out _);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _spriteView?.Dispose();
+            _spriteView = null;
+
+            TargetPreview?.RemoveAllChildren();
+        }
+
+        base.Dispose(disposing);
+    }
+}

Lytheriia/egoob

Content.Client/_Orion/DetailExaminable/DetailExaminableWindow.xaml.cs
@@ -1,0 +1,330 @@
+using System.Linq;
+using System.Numerics;
+using Content.Client.Stylesheets;
+using Content.Client.UserInterface.Controls;
+using Content.Shared._Orion.DetailExaminable;
+using Content.Shared.CCVar;
+using Content.Shared.Inventory;
+using Robust.Client.AutoGenerated;
+using Robust.Client.Player;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+using Robust.Shared.Configuration;
+
+namespace Content.Client._Orion.DetailExaminable;
+
+//
+// License-Identifier: GPL-3.0-or-later
+//
+
+[GenerateTypedNameReferences]
+public sealed partial class DetailExaminableWindow : FancyWindow
+{
+    private readonly List<Direction> _rotationSequence = new()
+    {
+        Direction.East,
+        Direction.North,
+        Direction.West,
+        Direction.South,
+    };
+
+    private int _currentDirectionIndex = 3;
+    private EntityUid? _currentEntity;
+    private SpriteView? _spriteView;
+
+    public DetailExaminableWindow()
+    {
+        RobustXamlLoader.Load(this);
+
+        RotateLeftButton.OnPressed += _ => RotateDirection(-1);
+        RotateRightButton.OnPressed += _ => RotateDirection(1);
+    }
+
+    private void RotateDirection(int step)
+    {
+        _currentDirectionIndex = (_currentDirectionIndex + step + _rotationSequence.Count) % _rotationSequence.Count;
+        UpdateSpriteDirection();
+    }
+
+    private void UpdateSpriteDirection()
+    {
+        if (_currentEntity == null || TargetPreview == null)
+            return;
+
+        TargetPreview.RemoveAllChildren();
+
+        _spriteView = new SpriteView // TODO: Better view for 32x48* sprites
+        {
+            OverrideDirection = _rotationSequence[_currentDirectionIndex],
+            Scale = new Vector2(8f, 8f),
+            //            MaxSize = new Vector2(64, 256), // This killing 32x48* sprites
+            MinSize = new Vector2(32, 128),
+            Stretch = SpriteView.StretchMode.Fit,
+        };
+
+        _spriteView.SetEntity(_currentEntity.Value);
+        TargetPreview.AddChild(_spriteView);
+    }
+
+    public void UpdateState(DetailExaminableEuiState state, IEntityManager entManager)
+    {
+        var config = IoCManager.Resolve<IConfigurationManager>();
+        _currentEntity = entManager.GetEntity(state.Target);
+
+        var isClothed = IsCharacterClothed(_currentEntity.Value, entManager);
+
+        PreviewTabs.SetTabTitle(0, Loc.GetString("flavor-tab-flavor"));
+        PreviewTabs.SetTabTitle(1, Loc.GetString("flavor-tab-character"));
+        PreviewTabs.SetTabTitle(2, Loc.GetString("flavor-tab-gyr"));
+        PreviewTabs.SetTabTitle(3, Loc.GetString("flavor-tab-nsfw"));
+
+        // Hide things if they are disabled in CCVars
+        var showTraits = config.GetCVar(CCVars.FlavorTraitsEnabled);
+        var showOoc = config.GetCVar(CCVars.FlavorOocEnabled);
+        var showGyr = config.GetCVar(CCVars.FlavorGyrEnabled);
+        var showNsfw = config.GetCVar(CCVars.FlavorNsfwEnabled) &&
+                       config.GetCVar(CCVars.NsfwContentEnabled) &&
+                       !isClothed;
+        var showLinks = config.GetCVar(CCVars.FlavorLinksEnabled);
+
+        PreviewTabs.SetTabVisible(0, showOoc);
+        PreviewTabs.SetTabVisible(1, showTraits);
+        PreviewTabs.SetTabVisible(2, showGyr);
+        PreviewTabs.SetTabVisible(3, showNsfw);
+
+        var species = Loc.GetString($"species-name-{state.Species.ToString().ToLower()}");
+        var sex = Loc.GetString($"humanoid-profile-editor-sex-{state.Sex.ToString().ToLower()}-text");
+        var gender = Loc.GetString($"humanoid-profile-editor-pronouns-{state.Gender.ToString().ToLower()}-text");
+
+        PreviewNameText.Text = state.Name;
+        PreviewGenderText.Text = $"{species} | {sex} | {gender}";
+
+        UpdateSpriteDirection();
+
+        PreviewAppearanceText.SetMessage(GetContentWithEmptyMessage(state.FlavorText, "detail-examinable-empty-flavor"));
+
+        if (showOoc)
+            PreviewOOCText.SetMessage(GetContentWithEmptyMessage(state.OOCFlavorText, "detail-examinable-empty-ooc"));
+
+        if (showTraits)
+            PreviewTraitsText.SetMessage(GetContentWithEmptyMessage(state.CharacterFlavorText, "detail-examinable-empty-character"));
+
+        if (showNsfw)
+            PreviewNSFWText.SetMessage(GetContentWithEmptyMessage(state.NSFWFlavorText, "detail-examinable-empty-nsfw"));
+
+        if (showGyr)
+        {
+            PreviewGYRContainer.RemoveAllChildren();
+
+            PreviewGYRContainer.AddChild(CreateSectionHeader("humanoid-profile-editor-gyr-green", Color.Green, useStripeBack: true));
+            CreateGYRTextLabel(GetContentWithEmptyMessage(state.GreenFlavorText, "detail-examinable-empty-green"));
+
+            PreviewGYRContainer.AddChild(CreateSectionHeader("humanoid-profile-editor-gyr-yellow", Color.Yellow, useStripeBack: true));
+            CreateGYRTextLabel(GetContentWithEmptyMessage(state.YellowFlavorText, "detail-examinable-empty-yellow"));
+
+            PreviewGYRContainer.AddChild(CreateSectionHeader("humanoid-profile-editor-gyr-red", Color.Red, useStripeBack: true));
+            CreateGYRTextLabel(GetContentWithEmptyMessage(state.RedFlavorText, "detail-examinable-empty-red"));
+        }
+
+        PreviewTagsText.Text = state.TagsFlavorText;
+
+        if (showLinks)
+        {
+            ProcessLinks(state.LinksFlavorText);
+        }
+        else // TODO: Remove all container, now its just remove links
+        {
+            PreviewLinksContainer?.RemoveAllChildren();
+        }
+
+        Badge.Visible = PlayerBadge();
+    }
+
+    private static string GetContentWithEmptyMessage(string content, string emptyMessageKey)
+    {
+        if (string.IsNullOrWhiteSpace(content))
+            return Loc.GetString(emptyMessageKey);
+
+        return content;
+    }
+
+    private void CreateGYRTextLabel(string text)
+    {
+        var label = new RichTextLabel
+        {
+            Text = text + "\n",
+            VerticalExpand = true,
+        };
+
+        PreviewGYRContainer.AddChild(label);
+    }
+
+    private void ProcessLinks(string linksText)
+    {
+        if (PreviewLinksContainer == null)
+            return;
+
+        PreviewLinksContainer.RemoveAllChildren();
+        if (string.IsNullOrEmpty(linksText))
+        {
+            var emptyLabel = new Label
+            {
+                Text = Loc.GetString("detail-examinable-empty-links"),
+                HorizontalExpand = true,
+                HorizontalAlignment = HAlignment.Center,
+                FontColorOverride = Color.Gray
+            };
+            PreviewLinksContainer.AddChild(emptyLabel);
+            return;
+        }
+
+        var links = linksText.Split(new[] { ',', ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var link in links)
+        {
+            if (IsValidUrl(link))
+            {
+                CreateLinkButton(link);
+            }
+            else
+            {
+                CreateLinkTextLabel(link);
+            }
+        }
+    }
+
+    private static bool IsValidUrl(string url)
+    {
+        return url.StartsWith("https://docs.google.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://disk.yandex.by", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://disk.yandex.ru", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://docs.yandex.by", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://docs.yandex.ru", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://ru.imgbb.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://gyazo.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://i.gyazo.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://e621.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://static1.e621.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://e926.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://static1.e926.net", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://ibb.co", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://steamcommunity.com/id/", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://steamcommunity.com/sharedfiles", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://images.steamusercontent.com", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://media.discordapp.net/attachments", StringComparison.OrdinalIgnoreCase) ||
+            url.StartsWith("https://cdn.discordapp.com/attachments", StringComparison.OrdinalIgnoreCase);
+    }
+
+    private void CreateLinkButton(string url)
+    {
+        var button = new Button
+        {
+            Text = GetLinkDisplayText(url),
+            ToolTip = Loc.GetString("humanoid-profile-editor-link-tooltip", ("url", url)),
+            HorizontalExpand = true,
+            HorizontalAlignment = HAlignment.Center,
+            StyleClasses = { StyleClass.ButtonOpenBoth },
+        };
+
+        button.OnPressed += _ => OpenLink(url);
+
+        PreviewLinksContainer.AddChild(button);
+    }
+
+    private void CreateLinkTextLabel(string text)
+    {
+        var label = new Label
+        {
+            Text = text,
+            HorizontalExpand = true,
+            HorizontalAlignment = HAlignment.Center,
+            FontColorOverride = Color.Gray,
+        };
+
+        PreviewLinksContainer.AddChild(label);
+    }
+
+    private static string GetLinkDisplayText(string url)
+    {
+        if (url.Length > 40)
+        {
+            return url[..37] + "...";
+        }
+        return url;
+    }
+
+    private static void OpenLink(string url)
+    {
+        if (url.StartsWith("www.", StringComparison.OrdinalIgnoreCase))
+            url = "https://" + url;
+
+        var uriOpener = IoCManager.Resolve<IUriOpener>();
+        uriOpener.OpenUri(url);
+    }
+
+    private Control CreateSectionHeader(string text, Color? color = null, bool useStripeBack = true)
+    {
+        var label = new Label
+        {
+            Text = Loc.GetString(text),
+            StyleClasses = { "LabelBig" },
+            Align = Label.AlignMode.Center,
+        };
+
+        if (color.HasValue)
+            label.FontColorOverride = color.Value;
+
+        if (useStripeBack)
+        {
+            var stripe = new StripeBack();
+            stripe.AddChild(label);
+            return stripe;
+        }
+
+        return label;
+    }
+
+    private bool PlayerBadge() // TODO: Something like donator, coder, owner badges
+    {
+        if (_currentEntity == null)
+            return false;
+
+        var playerManager = IoCManager.Resolve<IPlayerManager>();
+        if (!playerManager.TryGetSessionByEntity(_currentEntity.Value, out var session))
+            return false;
+
+        var playerName = session.Name;
+        var ckeyToMatch = playerName.StartsWith("localhost@", StringComparison.OrdinalIgnoreCase)
+            ? playerName.Substring("localhost@".Length)
+            : playerName;
+
+        var ckeyLower = ckeyToMatch.ToLowerInvariant();
+        var grantedCKeys = new[] { "puroslavking" };
+
+        return grantedCKeys.Contains(ckeyLower);
+    }
+
+    private bool IsCharacterClothed(EntityUid entity, IEntityManager entManager) // Hide NSFW flavor if character is clothed
+    {
+        if (entity == EntityUid.Invalid)
+            return false;
+
+        var inventorySystem = entManager.System<InventorySystem>();
+
+        return inventorySystem.TryGetSlotEntity(entity, "outerClothing", out _) ||
+               inventorySystem.TryGetSlotEntity(entity, "jumpsuit", out _);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _spriteView?.Dispose();
+            _spriteView = null;
+
+            TargetPreview?.RemoveAllChildren();
+        }
+
+        base.Dispose(disposing);
+    }
+}